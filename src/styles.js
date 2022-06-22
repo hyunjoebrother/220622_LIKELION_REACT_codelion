9e9669a4f900e6262e2212a5644f5e8b4324190c
hyunjoebrother <hyunjoe31@gmail.com>
2022-06-22 17:21:54 +0900

    feat: basic FE components - lecture week#1

--- src/styles.js
+++ src/styles.js
@@ -1,11 +1,17 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 //yarn add styled-reset
-import reset from 'styled-reset';
+import reset from "styled-reset";
 
-export const lightTheme = {};
+export const lightTheme = {
+  fontColor: "#2c2c2c",
+  bgColor: "white",
+};
 
-export const darkTheme = {};
+export const darkTheme = {
+  fontColor: "white",
+  bgColor: "#2c2c2c",
+};
 
 export const GlobalStyles = createGlobalStyle`
     ${reset}
